74dc3b9ac57dafe46c748d2473c0b9cc388343a4
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-08-15 21:56:27 +0530

    parts: Add support for LCD HBM and CABC
     * write preferences and qs tiles
     * kang icons from realmeparts
     * enable cabc by default
     * address sepolicy denials
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- pocoparts.mk
+++ pocoparts.mk
@@ -6,6 +6,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/parts/init.xiaomiparts.rc:$(TARGET_COPY_OUT_PRODUCT)/etc/init/init.xiaomiparts.rc \
     $(LOCAL_PATH)/parts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-parts.xml
 
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.lcd.cabc_mode=1
+
 # Sepolicy
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(LOCAL_PATH)/sepolicy/private
 BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy/vendor
