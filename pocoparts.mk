5a1cb76c400d4912e0538587a95eb5dc92bbaa15
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-09-05 01:38:40 -0300

    parts: on how permissions for the use of sepolicys
     * set sm6150 platform too
    
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- pocoparts.mk
+++ pocoparts.mk
@@ -5,3 +5,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/parts/init.xiaomiparts.rc:$(TARGET_COPY_OUT_PRODUCT)/etc/init/init.xiaomiparts.rc \
     $(LOCAL_PATH)/parts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-parts.xml
+
+# Sepolicy
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(LOCAL_PATH)/sepolicy/private
+BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy/vendor
+TARGET_SEPOLICY_DIR := msmsteppe
